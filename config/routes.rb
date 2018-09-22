b82995930eea6ce4e34c19bc8cd13009d79d5bbf
Adam Duff <j.adam.duff@gmail.com>
2018-09-22 03:05:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/coupons/:id', to: 'coupons#show', as: 'coupon'
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
