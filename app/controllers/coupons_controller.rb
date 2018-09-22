27704e4ef17f23af6d354ab7b61b66d7649dcb06
Adam Duff <j.adam.duff@gmail.com>
2018-09-22 03:29:58 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,1 +1,23 @@
+class CouponsController < ApplicationController
 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def show
+
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    if @coupon.save
+      redirect_to coupon_path(@coupon)
+    end
+  end
+end
