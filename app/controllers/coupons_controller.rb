b2ccb1999a95d947e250df11b1f46360043894d0
Adam Duff <j.adam.duff@gmail.com>
2018-09-22 03:12:19 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -13,6 +13,11 @@ class CouponsController < ApplicationController
   end
 
   def create
-
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    if @coupon.save
+      redirect_to coupon_path(@coupon)
+    end
   end
 end
